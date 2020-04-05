1a139c4600b18d3c8cad667e518a08ef0dd953b3
Aldo Caruso <aldocaruso1991@gmail.com>
2020-04-05 11:09:42 +0200

    update

--- defer.js
+++ defer.js
@@ -1,1 +1,10 @@
+Function.prototype.defer = function(ms){
+    return  setTimeout(this, ms) 
+}
+function delay(){
+    return console.log(`il ritardo è di: ${ms} ms (lanciare per credere!)`)
+}
+
+let ms = 999999999
+delay.defer(ms)
 
