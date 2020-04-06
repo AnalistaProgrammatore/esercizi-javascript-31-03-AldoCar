fc59c64c1ddf16dbded661a77bed4ae9978b90fb
Aldo Caruso <aldocaruso1991@gmail.com>
2020-04-06 15:42:39 +0200

    update

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 /**
 * Scrivere qui il codice della class Clock partendo dalla funzione qui di seguito definita
-**/
+
 function Clock({ template }) {
   let timer
   
@@ -37,3 +37,43 @@ function Clock({ template }) {
   
 let clock = new Clock({template: 'h:m:s'})
 clock.start()
+**/
+
+class Clock{
+    constructor ({ template }) {
+    let timer;
+    
+    
+    function render() {
+      let date = new Date()
+  
+      let hours = date.getHours()
+      if (hours < 10) hours = '0' + hours
+  
+      let mins = date.getMinutes()
+      if (mins < 10) mins = '0' + mins
+  
+      let secs = date.getSeconds()
+      if (secs < 10) secs = '0' + secs
+  
+      let output = template
+        .replace('h', hours)
+        .replace('m', mins)
+        .replace('s', secs)
+    
+      console.log(output)
+    }
+    
+    this.stop = function() {
+      clearInterval(timer)
+    }
+    
+    this.start = function() {
+      render();
+      timer = setInterval(render, 1000)
+    }
+    
+  }
+}  
+  let clock = new Clock({template: 'h:m:s'})
+  clock.start()
